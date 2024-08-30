a4e4c0e0493073d4d5f5f2cbab240bfd7bccfa45
r-kjha <mail@rkjha.com.np>
2024-08-30 22:29:26 +0545

    organized all

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)  # Create a Flask app
-app.secret_key = 'Gqrek7GREOk9Yu'  # Set a secret key for the session
+app.secret_key = os.getenv('SECRET_KEY')  # Set a secret key for the session
 
 # Configure MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'phpmyadmin'
-app.config['MYSQL_PASSWORD'] = '3850'
-app.config['MYSQL_DB'] = 'flask'
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 mysql = MySQL(app)  # Initialize MySQL
 
 @app.route('/')
@@ -66,4 +71,4 @@ def logout():
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0", debug=True)
